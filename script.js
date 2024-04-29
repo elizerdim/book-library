514d28e5a415e24569bf06438018ac39650957f3
Eliz Erdim <elizerdim@gmail.com>
2024-04-29 16:21:16 +0300

    Add Book constructor

--- script.js
+++ script.js
@@ -19,3 +19,10 @@ function displayBooks(books) {
 
 // TODO: Add functionality to book-read-btn buttons
 displayBooks(library);
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
