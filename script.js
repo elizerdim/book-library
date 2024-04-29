c3a23c31e1878296e118cd0414e2895be4942245
Eliz Erdim <elizerdim@gmail.com>
2024-04-29 16:20:54 +0300

    Add display books functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+const library = [];
+
+function displayBooks(books) {
+  const booksUI = document.getElementById('books');
+
+  books.forEach(
+    ({ title, author, pages, read }) => {
+      booksUI.innerHTML += `
+        <article class="book">
+          <h2 class="book-title">${title}</h2>
+          <p class="book-author">by ${author}</p>
+          <p class="book-pages">${pages} pages</p>
+          <button type="button" class="book-read-btn">${read ? "Read" : "Not read"}</button>
+        </article>
+      `
+    }
+  )
+}
+
+// TODO: Add functionality to book-read-btn buttons
+displayBooks(library);
